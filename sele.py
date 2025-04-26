c22277c4b5b1fcab81d6a0231ced7a455343a863
ShivRaiGithub <sshaktirai@gmail.com>
2025-04-26 07:49:32 +0000

    Jenkins

--- sele.py
+++ sele.py
@@ -1,6 +1,9 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-
-driver = webdriver.Chrome()
-driver.get('https://selenium.dev/')
-driver.quit()
+def test_open_site():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('https://selenium.dev/')
+    driver.quit()
